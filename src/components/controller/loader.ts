35bbddee4da259627ab43499c012f16c49daa786
Андрей <andrej@MacBook-Air.local>
2022-07-03 15:32:33 +0300

    feat:migrated the file Loader.js   to TS

--- src/components/controller/loader.ts
+++ src/components/controller/loader.ts
@@ -1,11 +1,16 @@
+import { IEverythingRequestParameters, endpointType, methodType, callback, Status, Request } from '../../types/';
+
+
 class Loader {
-    constructor(baseLink, options) {
+    private baseLink: string;
+    private options: IEverythingRequestParameters;
+    constructor(baseLink: string, options: IEverythingRequestParameters) {
         this.baseLink = baseLink;
         this.options = options;
     }
 
     getResp(
-        { endpoint, options = {} },
+        { endpoint, options = {} as Request }: { endpoint: endpointType; options?: Request },
         callback = () => {
             console.error('No callback for GET response');
         }
@@ -13,9 +18,9 @@ class Loader {
         this.load('GET', endpoint, callback, options);
     }
 
-    errorHandler(res) {
+    errorHandler(res: Response) {
         if (!res.ok) {
-            if (res.status === 401 || res.status === 404)
+            if (res.status === Status.One || res.status === Status.Two)
                 console.log(`Sorry, but there is ${res.status} error: ${res.statusText}`);
             throw Error(res.statusText);
         }
@@ -23,18 +28,18 @@ class Loader {
         return res;
     }
 
-    makeUrl(options, endpoint) {
-        const urlOptions = { ...this.options, ...options };
+    makeUrl(options: Request, endpoint: endpointType): string {
+        const urlOptions: Request = { ...this.options, ...options };
         let url = `${this.baseLink}${endpoint}?`;
 
         Object.keys(urlOptions).forEach((key) => {
-            url += `${key}=${urlOptions[key]}&`;
+            url += `${key}=${urlOptions[key as keyof Request]}&`;
         });
 
         return url.slice(0, -1);
     }
 
-    load(method, endpoint, callback, options = {}) {
+    load(method: methodType, endpoint: endpointType, callback: callback, options: Request) {
         fetch(this.makeUrl(options, endpoint), { method })
             .then(this.errorHandler)
             .then((res) => res.json())
