ad96fcc32ab86e79f3260c5bcd75bbf13e3aa63e
Андрей <andrej@MacBook-Air.local>
2022-07-03 14:09:35 +0300

    feat:added keys in the file appLoader.js

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '6ada2a06577644a7ad815c5b796e3107', // получите свой ключ https://newsapi.org/
         });
     }
 }
