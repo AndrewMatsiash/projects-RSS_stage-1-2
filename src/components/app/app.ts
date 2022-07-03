52a6ce338ffbe2e9c9b91e75697de982e42a013d
Андрей <andrej@MacBook-Air.local>
2022-07-03 15:45:28 +0300

    feat:migrated the file app.js   to TS

---
+++ src/components/app/app.ts
@@ -0,0 +1,21 @@
+import AppController from '../controller/controller';
+import { AppView } from '../view/appView';
+import { IResponceEverything, IResponceSources } from '../../types/';
+
+
+class App {
+    private controller = new AppController
+    private view = new AppView
+    constructor() {
+        this.controller = new AppController();
+        this.view = new AppView();
+    }
+
+    start() {
+        const sourcesElement: HTMLElement = document.querySelector('.sources') as HTMLElement
+        sourcesElement.addEventListener('click', (e) => this.controller.getNews(e, (data) => this.view.drawNews(data as IResponceEverything)));
+        this.controller.getSources((data) => this.view.drawSources(data as IResponceSources));
+    }
+}
+
+export default App;
