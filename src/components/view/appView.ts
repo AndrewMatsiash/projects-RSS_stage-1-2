9bf647c32e14f40d18afdff354ef8c7d9b2f9f4c
Андрей <andrej@MacBook-Air.local>
2022-07-03 15:30:03 +0300

    feat:migrated the file AppView.js   to TS

---
+++ src/components/view/appView.ts
@@ -0,0 +1,23 @@
+import News from './news/news';
+import Sources from './sources/sources';
+import { IResponceEverything, IResponceSources, ISourcesSource, IArticles } from '../../types/';
+export class AppView {
+    news: News
+    sources: Sources;
+    constructor() {
+        this.news = new News();
+        this.sources = new Sources();
+    }
+
+    drawNews(data: IResponceEverything): void {
+        const values: IArticles[] = data?.articles ? data?.articles : [];
+        this.news.draw(values);
+    }
+
+    drawSources(data: IResponceSources): void {
+        const values: ISourcesSource[] = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
