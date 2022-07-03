665929d8bc45ae47e628ae2d861a2cb0e4a2d603
Андрей <andrej@MacBook-Air.local>
2022-07-03 15:39:45 +0300

    feat:migrated the file controller.js   to TS

--- src/components/controller/controller.ts
+++ src/components/controller/controller.ts
@@ -1,7 +1,8 @@
+import { callback } from '../../types/types';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: callback): void {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,28 +11,26 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: callback): void {
+        let target = e.target as HTMLElement;
+        const newsContainer: HTMLElement = e.currentTarget as HTMLElement;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId: string = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
                         {
                             endpoint: 'everything',
-                            options: {
-                                sources: sourceId,
-                            },
+                            options: { sources: sourceId }
                         },
                         callback
                     );
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
