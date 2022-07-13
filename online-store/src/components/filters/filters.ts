1153d50fc63981834eeea7a5a5565ac78789d90c
Андрей <andrej@MacBook-Air.local>
2022-07-13 18:12:17 +0300

    feat: made a product filter by value

--- online-store/src/components/filters/filters.ts
+++ online-store/src/components/filters/filters.ts
@@ -1,29 +1,53 @@
 import { CATALOG } from '../../constants/catalog';
 import { productsPage } from '../products/products';
 
-const buttonsBrand = document.querySelectorAll('.btn-brand');
+const buttonsFilterBrand = document.querySelectorAll('.btn-brand');
+const buttonsFilterColor = document.querySelectorAll('.btn-color');
+const buttonsFilterCamaras = document.querySelectorAll('.btn-cameras');
+const buttonsFilterPopular = document.querySelectorAll('.btn-popular');
+const buttonsFilter = document.querySelectorAll('.btn-filter')
+
 
 class Filters {
   classList: any;
 
-  renderSortBrand():void {
-    this.classList.toggle('btn_active');
 
-    const brandsForFilter:string[] = Array.from(buttonsBrand)
+  filters() {
+    let catalog = CATALOG;
+    this.classList.toggle('btn_active')
+
+    let filterBrandArr = filters.filterValues(buttonsFilterBrand)
+    let filterByCamerasArr = filters.filterValues(buttonsFilterCamaras)
+    let filterByColorArr = filters.filterValues(buttonsFilterColor)
+    let filterByPopular = filters.filterValues(buttonsFilterPopular)
+
+    if (filterBrandArr.length !== 0) {
+      catalog = catalog.filter((el) => filterBrandArr.includes(el.brand));
+    }
+    if (filterByColorArr.length !== 0) {
+      catalog = catalog.filter((el) => filterByColorArr.includes(el.color));
+    }
+    if (filterByCamerasArr.length !== 0) {
+      catalog = catalog.filter((el) => filterByCamerasArr.includes(el.numberOfCameras.toString()));
+    }
+    if (filterByPopular.length !== 0) {
+      catalog = catalog.filter((el) => filterByPopular.includes(el.popular));
+    }
+
+    productsPage.render(catalog)
+  }
+
+  filterValues(buttonsFilter:NodeListOf<Element>) {
+    const forFilter: string[] = Array.from(buttonsFilter)
       .filter((btn) => btn.classList.contains('btn_active'))
       .map((btn) => btn.innerHTML);
-
-    if (brandsForFilter.length !== 0) {
-      const sortBrand = CATALOG.filter((el) => brandsForFilter.includes(el.brand));
-      productsPage.render(sortBrand);
-    } else {
-      productsPage.render();
-    }
+    return forFilter
   }
+
 }
 
 const filters = new Filters();
 
-buttonsBrand.forEach((el) => {
-  el.addEventListener('click', filters.renderSortBrand);
+buttonsFilter.forEach((el) => {
+  el.addEventListener('click', filters.filters);
 });
