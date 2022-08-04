153ca08e76e776020be20f9ad47bab45aee3d0f3
Андрей <andrej@MacBook-Air.local>
2022-08-04 19:20:56 +0300

    feat:added listen select  car by click select-btn

--- async-race/src/components/listeners.ts
+++ async-race/src/components/listeners.ts
@@ -1,5 +1,6 @@
 import { createCar } from "../request/createCar";
 import { deleteCar } from "../request/deletCar";
+import { getCar } from "../request/getCar";
 import { renderGarage } from "./garage/garage";
 import { updateStateGarage } from "./garage/updateGarage";
 
@@ -33,6 +34,20 @@ export const listen = () => {
         await updateStateGarage()
         const ROOT_GARAGE = document.querySelector('.garage-wrapper') as HTMLElement;
         ROOT_GARAGE.innerHTML = renderGarage()
+      }
+      if (event.target.classList.contains("select-btn")) {
+        const idCar: number = +event.target.id.split('select-car-')[1]
+        const selectedCar = await getCar(idCar)
+
+        const formUpdate = document.getElementById(('update-name')) as HTMLFormElement
+        const formUpdateColor = document.getElementById('update-color') as HTMLFormElement
+        const formUpdateBtn = document.getElementById('update-btn') as HTMLFormElement
+        formUpdate.setAttribute('data-id', idCar.toString())
+        formUpdate.value = selectedCar.name;
+        formUpdateColor.value = selectedCar.color;
+        formUpdate.disabled = false;
+        formUpdateColor.disabled = false;
+        formUpdateBtn.disabled = false;
       }
     }
   })
